8a34ca270de5fe1fb511397b700b79d4dd0701dc
Daniel Figueroa <danielmfigueroa@gmail.com>
2020-09-23 11:18:40 -0300

    User input

--- FRP3/covid-brasil.js
+++ FRP3/covid-brasil.js
@@ -1,4 +1,22 @@
-fetch('https://covid19-brazil-api.now.sh/api/report/v1/brazil/uf/go')
-  .then( res => res.json() )
-  .then( data => console.log(data) )
-  .catch(error => console.log('Conexão Falhou'))
+function covid() {
+  const ufInput = document.getElementById('userUf').value
+
+  const api_url = 'https://covid19-brazil-api.now.sh/api/report/v1/brazil/uf/' + ufInput
+
+  async function getInfoCovid() {
+    const response = await fetch(api_url)
+    const data = await response.json()
+    const { uf, cases, deaths, suspects, refuses, datetime } = data
+
+    document.getElementById('uf').textContent = uf
+    document.getElementById('cases').textContent = cases
+    document.getElementById('deaths').textContent = deaths
+    document.getElementById('suspects').textContent = suspects
+    document.getElementById('refuses').textContent = refuses
+    document.getElementById('datetime').textContent = datetime
+
+  }
+  getInfoCovid()
+}
+
+
